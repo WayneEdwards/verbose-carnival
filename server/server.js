aa0a60d4ad8434e78d86c4dd662d561c6cb04007
Wayne Edwards <waedwards42@yahoo.com>
2021-07-29 15:21:29 -0700

    edit server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-
 const { ApolloServer } = require('apollo-server-express');
-
 const path = require('path');
-const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const { typeDefs, resolvers } = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({ 
   typeDefs, 
   resolvers, 
@@ -25,8 +26,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
