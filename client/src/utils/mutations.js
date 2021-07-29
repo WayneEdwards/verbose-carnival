4f38615602a1fece4a9e637660aa8ca1e7c818de
Wayne Edwards <waedwards42@yahoo.com>
2021-07-29 14:56:42 -0700

    update front-end-specs

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,65 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+mutation login(email: String! password: String!) {
+    login(email: $email, password: $password) {
+        user {
+            _id
+            username
+            email
+        }
+        token
+    }
+}
+
+`;
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $password: String!, $email: String!) {
+        addUser(username: $username, password: $password, email: $email) {
+            user {
+                _id
+                username
+                email
+            }
+            token 
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookData: bookData!) {
+        saveBook(bookData: $bookData) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                description
+                title
+                image
+                link
+                authors
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation deleteBook ($bookId: String!) {
+        deleteBook(bookId:$bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                description
+                title
+                image
+                link
+                authors
+            }
+        }
+    }
+`;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,22 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+{
+    me {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            _id
+            authors
+            description
+            bookId
+            image
+            link
+            title
+        }
+    }
+}
+
+`;
